808bd926c7951f96bc52687e1e27c64e36f42010
wilmer Borjas <wilmer.aprendiendo.programacion@gmail.com>
2024-04-29 09:55:24 +0200

    se crea nueva clase animal  y sus atributos

---
+++ src/main/java/com/wbg/app/animal/Perro.java
@@ -0,0 +1,33 @@
+package com.wbg.app.animal;
+
+public class Perro {
+
+	private String nombre;
+	private int edad;
+	private double peso;
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public void setNombre(int edad) {
+		this.edad = edad;
+	}
+
+	public int getEdad() {
+		return this.edad;
+	}
+
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+
+	public double getPeso() {
+		return this.peso;
+	}
+
+}
---
+++ src/main/java/com/wbg/app/animal/Vaca.java
@@ -0,0 +1,5 @@
+package com.wbg.app.animal;
+
+public class Vaca {
+
+}
