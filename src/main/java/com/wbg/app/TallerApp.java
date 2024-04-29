07bad48f5bce4efbcc922dffddd30a6a7ec102ca
wilmer Borjas <wilmer.aprendiendo.programacion@gmail.com>
2024-04-29 10:16:50 +0200

    se crea nueva clase animal  y sus atributos

--- src/main/java/com/wbg/app/TallerApp.java
+++ src/main/java/com/wbg/app/TallerApp.java
@@ -1,11 +1,14 @@
 package com.wbg.app;
 
+import com.wbg.app.animal.Perro;
+
 public class TallerApp {
 	public static void main(String[] args) {
-		
-		System.out.println("volviendo a clases ");
-		
-		
-	}
 
+		System.out.println("volviendo a clases ");
+
+	}
+	
+	Perro perro = new Perro();
+	 
 }
--- src/main/java/com/wbg/app/animal/Vaca.java
+++ src/main/java/com/wbg/app/animal/Vaca.java
@@ -2,4 +2,33 @@ package com.wbg.app.animal;
 
 public class Vaca {
 
+	private String raza;
+	private int cantidadCuerno;
+	private double tamanio;
+
+	public void setRaza(String raza) {
+		this.raza = raza;
+	}
+
+	public String getraza() {
+		return this.raza;
+	}
+
+	public void setcantidadCuerno(int cantidadCuerno) {
+		this.cantidadCuerno = cantidadCuerno;
+	}
+
+	public int getcantidadCuerno() {
+		return this.cantidadCuerno;
+	}
+
+	public void settamanio(double tamanio) {
+		this.tamanio = tamanio;
+	}
+
+	public double gettamanio() {
+		return this.tamanio;
+
+	}
+
 }
